763f1f69c87fbde9743cccfa0b27228cb8d686d2
Gopalakrishnan Iyer <gopalakrishnan.iyer@aexp.com>
2016-02-13 23:50:30 -0500

    Main screen commit

--- app/src/main/java/com/giyer7/dropanote/ActivityMain.java
+++ app/src/main/java/com/giyer7/dropanote/ActivityMain.java
@@ -1,13 +1,38 @@
 package com.giyer7.dropanote;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class ActivityMain extends AppCompatActivity {
+import com.bumptech.glide.Glide;
 
+public class ActivityMain extends AppCompatActivity implements View.OnClickListener{
+
+    ImageView iv_logo,iv_background;
+    Toolbar mToolbar;
+    Button mAddButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        iv_logo = (ImageView) findViewById(R.id.img_logo);
+        iv_background = (ImageView) findViewById(R.id.img_background);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mAddButton = (Button) findViewById(R.id.btn_dropanote);
+        mAddButton.setOnClickListener(this);
+        initBackgroundImage();
+        setSupportActionBar(mToolbar);
+    }
+
+    private void initBackgroundImage() {
+        Glide.with(this).load(R.drawable.background).centerCrop().into(iv_background);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
